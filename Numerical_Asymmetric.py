e9de8e58ca2f804437c92f1adadfec39d0755096
Patty <patricia.apostol@gmail.com>
2019-03-06 09:40:59 +0100

    Updated asymmetric

--- Numerical_Asymmetric.py
+++ Numerical_Asymmetric.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
 """
 Created on Mon Mar  4 17:16:42 2019
 
@@ -7,10 +7,31 @@ Created on Mon Mar  4 17:16:42 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
-def getStateSpace(C1,C2,C3):
+def getStateSpaceGeneral(C1,C2,C3):
     
     #This function takes the analytically determiner C1, C2 and C3 as inputs
     #Outputs the state space model in the following form:
     ################# x_dot = Ax+Bu
     ################# y=Cx+Du
+    A=-C.inv.multiply(C2)
+    B=-C.inv.multiply(C3)
+    
+    return A,B
+    
+    
+
+###MAIN###
+
+
+#C1=sp.Matrix([[CYb,0, 0, 0],[0,-0.5*(b/V),0,0],[0,0,-4*mub*KX2,4*mub*KXZ*(b/(2*V))],[Cnb*(b/V),0,4*mub*KXZ*(b/2V),-4*mub*KZ2*(b/2V)]])
+
+sp.var('a,b,c,d,e,f,g,h,r,')
+
+test1=sp.Matrix([a,b],[c,d])
+test2=sp.Matrix([e,f],[g,h])
+test3=sp.Matrix([g,r],[f,d])
+
+out=getStateSpaceGeneral(test1,test2,test3)
+print(out)
