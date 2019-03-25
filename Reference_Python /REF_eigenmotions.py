8560f6f72f95c2166e0b2edb5e8b22b838b7e016
Edmundo <mundi.sangadea@gmail.com>
2019-03-25 15:12:39 +0100

    :)

--- Reference_Python /REF_eigenmotions.py
+++ Reference_Python /REF_eigenmotions.py
@@ -8,12 +8,12 @@ Created on Fri Mar 15 14:44:51 2019
 
 #Store the time of eigen motions in an array
 
-AperiodicRoll = 51*60 + 13
-ShortPeriod = 52*60 + 3
-Phugoid = 53*60 + 25
-DutchRoll = 56*60 + 14
-DutchRollYD = 57*60 + 7
-Spiral = 1*3600 + 1*60 + 10
+AperiodicRoll = 59*60 + 10
+ShortPeriod = 1*3600 + 35
+Phugoid = 53*60 + 57
+DutchRoll = 1*3600 + 1*60 + 57
+DutchRollYD = 1*3600 + 2*60 + 47
+Spiral = 1*3600 + 5*60 + 20
 
 times = [AperiodicRoll, ShortPeriod, Phugoid, DutchRoll, DutchRollYD, Spiral]
 order = ['AperiodicRoll', 'ShortPeriod', 'Phugoid', 'DutchRoll', 'DutchRollYD', 'Spiral']
